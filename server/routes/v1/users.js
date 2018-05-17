8673b0b7764f1d2b4bc819a8ed45692c27577cbf
faksam <fakunlesamuel@gmail.com>
2018-05-17 11:38:24 +0100

    create server

---
+++ server/routes/v1/users.js
@@ -0,0 +1,11 @@
+// import passport from 'passport';
+// import { verrifyUserExist, verifySignInInput,verrifyUserCanSignIn, verifySignUpInput } from '../../helpers/userValidator';
+// import Authentication from '../../controllers/authController';
+
+
+// const requireSignIn = passport.authenticate('local', { session: false });
+
+// module.exports = (app) => {
+//   app.post('/auth/login',verifySignInInput,verrifyUserCanSignIn, requireSignIn, Authentication.login);
+//   app.post('/auth/signup',verifySignUpInput, verrifyUserExist, Authentication.signup);
+// };
