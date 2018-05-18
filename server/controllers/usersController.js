df074b31f78d086e769c5c13e5d419db54eed564
faksam <fakunlesamuel@gmail.com>
2018-05-18 10:05:35 +0100

    create api to get all requests

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -7,6 +7,33 @@ import usersRequest from '../db/usersRequest.json';
  * @export
  */
 export default class usersController {
+/**
+   * @description - Get all Requests
+   * @static
+   *
+   * @param {object} req - HTTP Request
+   * @param {object} res - HTTP Response
+   *
+   * @memberOf usersController
+   *
+   * @returns {object} Class Instance
+   */
+  static getRequests(req, res) {
+    /**
+     * @description - Finds request
+     * @param {request} item - UserRequest
+     *
+     * @returns {boolean} Class Instance
+     */
+    function filterByUserID(item) {
+      if (item.userId === 1) {
+        return true;
+      }
+      return false;
+    }
+    const currentUserRequests = usersRequest.requests.filter(filterByUserID);
+    res.status(200).send({ currentUserRequests });
+  }
 
 
 }
