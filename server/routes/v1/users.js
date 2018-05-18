58ddd893419c53e2a92de94641c15b02b627dc87
faksam <fakunlesamuel@gmail.com>
2018-05-18 10:02:47 +0100

    setup api versioning

--- server/routes/v1/users.js
+++ server/routes/v1/users.js
@@ -2,8 +2,6 @@ import usersCtrl from '../../controllers/usersController';
 import { verifyRequestInput, verifyIfRequestExist } from '../../helpers/validator';
 
 module.exports = (app) => {
-  app.get('/users/requests', usersCtrl.getRequests);
-  app.get('/users/requests/:id', verifyIfRequestExist, usersCtrl.getRequest);
-  app.post('/users/requests', verifyRequestInput, usersCtrl.postRequest);
-  app.put('/users/requests/:id', verifyIfRequestExist, verifyRequestInput, usersCtrl.updateRequest);
+  
+
 };
