275de36d1541929f498df7f2f2aed47021e84e3b
faksam <fakunlesamuel@gmail.com>
2018-05-18 10:05:35 +0100

    create api to get all requests

--- server/routes/v1/users.js
+++ server/routes/v1/users.js
@@ -2,6 +2,6 @@ import usersCtrl from '../../controllers/usersController';
 import { verifyRequestInput, verifyIfRequestExist } from '../../helpers/validator';
 
 module.exports = (app) => {
-  
+  app.get('/users/requests', usersCtrl.getRequests);
 
 };
