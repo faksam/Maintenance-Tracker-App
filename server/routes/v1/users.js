a79cd9d589c0d57255c6e1cc2cdb50c6e650dc0c
faksam <fakunlesamuel@gmail.com>
2018-05-23 23:36:06 +0100

    add validator to check if request is new

--- server/routes/v1/users.js
+++ server/routes/v1/users.js
@@ -1,9 +1,9 @@
 import usersController from '../../controllers/usersController';
-import { verifyRequestInput, verifyIfRequestExist } from '../../helpers/validator';
+import { verifyRequestInput, verifyIfRequestExist, checkRequestStatus } from '../../helpers/validator';
 
 module.exports = (app) => {
   app.get('/users/requests', usersController.getRequests);
   app.get('/users/requests/:id', verifyIfRequestExist, usersController.getRequest);
   app.post('/users/requests', verifyRequestInput, usersController.postRequest);
-  app.put('/users/requests/:id', verifyIfRequestExist, verifyRequestInput, usersController.updateRequest);
+  app.put('/users/requests/:id', verifyIfRequestExist, checkRequestStatus, verifyRequestInput, usersController.updateRequest);
 };
