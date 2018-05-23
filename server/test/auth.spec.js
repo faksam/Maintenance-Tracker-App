ea8fbb039b68d9f39ed5a6cf0d7ede36e2baffe0
faksam <fakunlesamuel@gmail.com>
2018-05-23 11:07:23 +0100

    remove unused/commented logic

---
+++ server/test/auth.spec.js
@@ -0,0 +1,95 @@
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../app';
+
+import users from './mock-data/user.json';
+
+chai.use(chaiHttp);
+
+describe('API endpoint /auth/signin & /auth/signup', () => {
+  before(() => {
+
+  });
+
+  after(() => {
+
+  });
+  // POST - Signup existing user
+  it('should not signup existing user', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(users.user[0])
+      .then((res) => {
+        expect(res).to.have.status(409);
+        expect(res.body).to.be.an('object');
+        expect(res.body.error.message).to.equal('This email/user already exist in our server, try signing in.');
+        done();
+      });
+  });
+
+  // POST - Signup user
+  it('it should add new user', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(users.user[1])
+      .then((res) => {
+        expect(res).to.have.status(201);
+        expect(res.body).to.be.an('object');
+        expect(res.body.data.fullName).to.equal(users.user[1].fullName);
+        expect(res.body.data.email).to.equal(users.user[1].email);
+        done();
+      });
+  });
+
+  // POST - Signup user
+  it('should return 400 Bad Request', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(users.user[2])
+      .then((res) => {
+        expect(res).to.have.status(400);
+        expect(res.body).to.be.an('object');
+        expect(res.body.error.message.fullName).to.equal('Full Name is required');
+        done();
+      });
+  });
+
+  // POST - Signup user
+  it('should return 400 Bad Request', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(users.user[3])
+      .then((res) => {
+        expect(res).to.have.status(400);
+        expect(res.body).to.be.an('object');
+        expect(res.body.error.message.email).to.equal('Email does not appear to be valid');
+        done();
+      });
+  });
+
+  // POST - Signup user
+  it('should return 400 Bad Request', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(users.user[4])
+      .then((res) => {
+        expect(res).to.have.status(400);
+        expect(res.body).to.be.an('object');
+        expect(res.body.error.message.phoneNo).to.equal('Phone No is required');
+        done();
+      });
+  });
+
+  // POST - Signup user
+  it('should return 400 Bad Request', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(users.user[5])
+      .then((res) => {
+        expect(res).to.have.status(400);
+        expect(res.body).to.be.an('object');
+        expect(res.body.error.message.password).to.equal('Password is required');
+        done();
+      });
+  });
+});
