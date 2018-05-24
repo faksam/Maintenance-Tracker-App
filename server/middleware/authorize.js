cf90cc41b54e37a74a1e4c668f06b4f9d10cb7ea
faksam <fakunlesamuel@gmail.com>
2018-05-24 01:02:32 +0100

    [Feature] create all admin api endpoints

--- server/middleware/authorize.js
+++ server/middleware/authorize.js
@@ -1,30 +1,61 @@
 import { Pool } from 'pg';
+import dotenv from 'dotenv';
 import { verifyToken } from '../helpers/validator';
 
-const pool = new Pool();
+dotenv.config();
+
+const env = process.env.NODE_ENV;
+let connectionString;
+
+if (env === 'development') {
+  connectionString = process.env.DATABASE_URL;
+} else {
+  connectionString = process.env.use_env_variable;
+}
+
 
 export const authorizeAdmin = (req, response, next) => {
   const decode = verifyToken(req, response, next);
 
-  pool.query('SELECT * FROM users WHERE id = $1', parseInt(decode.sub, 10), (err, result) => {
+  const pool = new Pool({
+    connectionString,
+  });
+  const queryValues = [];
+  queryValues.push(decode.sub);
+  pool.query('SELECT * FROM users WHERE id = $1', [queryValues[0]], (err, result) => {
     if (err) {
       throw err;
     }
-
     if (result.rows[0].role === 'Admin') { return next(); }
-    response.status(403).send({ error: 'You are Forbidden.' });
+    response.status(403).send({
+      success: false,
+      status: 403,
+      error: {
+        message: 'You are Forbidden.'
+      },
+    });
   });
 };
 
 export const authorizeUser = (req, res, next) => {
   const decode = verifyToken(req, res, next);
 
-  pool.query('SELECT * FROM users WHERE id = $1', parseInt(decode.sub, 10), (err, result) => {
+  const pool = new Pool({
+    connectionString,
+  });
+  const queryValues = [];
+  queryValues.push(decode.sub);
+  pool.query('SELECT * FROM users WHERE id = $1', [queryValues[0]], (err, result) => {
     if (err) {
       throw err;
     }
-
     if (result.rows[0].role.length > 3) { return next(); }
-    res.status(403).send({ error: 'You are not authorized.' });
+    res.status(401).send({
+      success: false,
+      status: 401,
+      error: {
+        message: 'You are not authorized.',
+      },
+    });
   });
 };
