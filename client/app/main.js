5553f17c0991d1bf607bbd642385ef8ff06d3839
faksam <fakunlesamuel@gmail.com>
2018-06-03 22:41:30 +0100

    [Feature #158056050] logout user

--- client/app/main.js
+++ client/app/main.js
@@ -1,10 +1,10 @@
 const checkLoggedInUser = () => {
-    const userRole = sessionStorage.getItem('user_role');
-    if (userRole === 'User') {
-        window.location = './homepage.html';
-      } else if (userRole === 'Admin') {
-        window.location = './adminhomepage.html';
-      } 
-}
+  const userRole = sessionStorage.getItem('user_role');
+  if (userRole === 'User') {
+    window.location = './homepage.html';
+  } else if (userRole === 'Admin') {
+    window.location = './adminhomepage.html';
+  }
+};
 
 window.onload = checkLoggedInUser();
