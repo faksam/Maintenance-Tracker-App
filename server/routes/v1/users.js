c655a8b06ca7ebfd4fa9f29d0c23427bfed338d5
faksam <fakunlesamuel@gmail.com>
2018-06-06 12:24:28 +0100

    Feature #158058640] View Account Details

---
+++ server/routes/v1/users.js
@@ -0,0 +1,12 @@
+import usersController from '../../controllers/usersController';
+import { verifyRequestInput, verifyUserToken, verifyUserRequest, checkIfUserRequestExist, checkRequestStatus } from '../../helpers/validator';
+import { authorizeUser } from '../../middleware/authorize';
+import { checkDuplicateRequest } from '../../helpers/userValidator';
+
+export default (app) => {
+  app.get('/users/account', verifyUserToken, authorizeUser, usersController.getUserDetails);
+  app.get('/users/requests', verifyUserToken, authorizeUser, usersController.getRequests);
+  app.get('/users/requests/:id', verifyUserToken, authorizeUser, verifyUserRequest, checkIfUserRequestExist, usersController.getRequest);
+  app.post('/users/requests', verifyUserToken, authorizeUser, verifyRequestInput, checkDuplicateRequest, usersController.postRequest);
+  app.put('/users/requests/:id', verifyUserToken, authorizeUser, verifyUserRequest, checkIfUserRequestExist, checkRequestStatus, verifyRequestInput, usersController.updateRequest);
+};
