4e47598bab8b99c951bd90c5721f8ae7bde627af
faksam <fakunlesamuel@gmail.com>
2018-06-06 12:30:23 +0100

    Feature #158058640] View Account Details

--- client/app/account/accountDetails.js
+++ client/app/account/accountDetails.js
@@ -6,23 +6,23 @@ const token = `Bearer ${sessionStorage.getItem('token')}`;
  *
  */
 const displayAccountDetails = () => {
-  const { fullName, email, phoneNo, role } = accountDetailsForm.elements;
+  const {
+    fullName, email, phoneNo, role,
+  } = accountDetailsForm.elements;
   const url = '/api/v1/users/account';
 
-    const fetchData = {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: token,
-        },
-    };
+  const fetchData = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: token,
+    },
+  };
 
   fetch(url, fetchData)
     .then(resp => resp.json())
     .then((body) => {
-        console.log(body);
       if (body.status === 200 && body.success === true) {
-        console.log(fullName)
         fullName.value = body.data.fullName;
         email.value = body.data.email;
         phoneNo.value = body.data.phoneNo;
